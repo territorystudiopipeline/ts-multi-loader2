6147bcbf2a726aec1b8b21dbb6c1ba497e515e27
dianaflorea-territory <diana.florea@territorystudio.com>
2026-04-22 16:59:12 +0100

    Merge pull request #2
    * add filtering expression
    
    * change to new entities fields
    
    * update for each context to accept anything that's directly linked to it
    
    * update for each context to accept anything that's indirectly linked t…
    
    * add color support for indirect published files
    
    * change pprint import position

--- python/tk_multi_loader/utils.py
+++ python/tk_multi_loader/utils.py
@@ -10,7 +10,7 @@
 
 import sgtk
 from sgtk.platform.qt import QtCore, QtGui
-
+import pprint
 
 class ResizeEventFilter(QtCore.QObject):
     """
@@ -261,29 +261,23 @@ def filter_publishes(app, sg_data_list):
         # Constructing a wrapper dictionary so that it's future proof to
         # support returning additional information from the hook
         hook_publish_list = [{"sg_publish": sg_data} for sg_data in sg_data_list]
-
-        hook_publish_list = app.execute_hook(
-            "filter_publishes_hook", publishes=hook_publish_list
-        )
-        if not isinstance(hook_publish_list, list):
-            app.log_error(
-                "hook_filter_publishes returned an unexpected result type \
-                '%s' - ignoring!"
-                % type(hook_publish_list).__name__
-            )
-            hook_publish_list = []
-
-        # split back out publishes:
-        sg_data_list = []
-        for item in hook_publish_list:
-            sg_data = item.get("sg_publish")
-            if sg_data:
-                sg_data_list.append(sg_data)
-
+        hook_publish_list = app.execute_hook("filter_publishes_hook", publishes=hook_publish_list)
+        sg_data_list = [item["sg_publish"] for item in hook_publish_list if item.get("sg_publish")]
     except:
         app.log_exception("Failed to execute 'filter_publishes_hook'!")
-        sg_data_list = []
 
+    related = _get_related_entities(app)
+    direct_lookup = {
+        (ent["type"], ent["id"]): ent.get("_is_direct", False)
+        for ent in related
+    }
+
+    for sg_data in sg_data_list:
+        entity = sg_data.get("entity")
+        if entity:
+            is_direct = direct_lookup.get((entity["type"], entity["id"]))
+            if is_direct is not None:
+                sg_data["_is_direct"] = is_direct  
     return sg_data_list
 
 
@@ -325,6 +319,110 @@ def resolve_filters(filters):
                     field = app.context.task
                 elif field == "{context.user}":
                     field = app.context.user
+                elif field == "{context.related_entities}":
+                    field = _get_related_entities(app)
                 resolved_filter.append(field)
         resolved_filters.append(resolved_filter)
     return resolved_filters
+
+
+def _append_entities(related, value):
+    """Append a single entity or extend a list of entities."""
+    if not value:
+        return
+    if isinstance(value, list):
+        related.extend(value)
+    else:
+        related.append(value)
+
+
+def _fetch_indirect_links(sg, entity_type, entity_id):
+    """
+    For Sequence and CustomEntity04 entities, fetch their own linked entities
+    and return them as indirect relations.
+    """
+    SEQUENCE_INDIRECT_FIELDS = ["sg_linked_shots", "sg_linked_assets", "sg_linked_master_assets"]
+    MASTER_ASSET_INDIRECT_FIELDS = ["sg_linked_shots", "sg_linked_assets", "sg_linked_sequences"]
+
+    indirect = []
+
+    if entity_type == "Sequence":
+        seq = sg.find_one("Sequence", [["id", "is", entity_id]], SEQUENCE_INDIRECT_FIELDS)
+        if seq:
+            for field in SEQUENCE_INDIRECT_FIELDS:
+                _append_entities(indirect, seq.get(field))
+
+    elif entity_type == "CustomEntity04":
+        master = sg.find_one("CustomEntity04", [["id", "is", entity_id]], MASTER_ASSET_INDIRECT_FIELDS)
+        if master:
+            for field in MASTER_ASSET_INDIRECT_FIELDS:
+                _append_entities(indirect, master.get(field))
+
+    return indirect
+
+
+def _resolve_indirect_links(sg, entities):
+    """
+    Given a list of entities, check if any are Sequence or CustomEntity04
+    and if so, fetch their linked entities too.
+    """
+    indirect = []
+    for entity in entities:
+        if not entity:
+            continue
+        _append_entities(indirect, _fetch_indirect_links(sg, entity["type"], entity["id"]))
+    return indirect
+
+
+def _get_related_entities(app):
+    context = app.context
+    sg = app.shotgun
+
+    if not context.entity:
+        return []
+
+    entity_type = context.entity["type"]
+    entity_id = context.entity["id"]
+
+    entity_list = ["sg_linked_sequences", "sg_linked_shots", "sg_linked_assets", "sg_linked_master_assets"]
+
+    related_dict = {}
+
+    def _add_entity(entity, is_direct, source_field, source_entity=None):
+        if entity and isinstance(entity, dict) and "type" in entity and "id" in entity:
+            key = (entity["type"], entity["id"])
+            if key not in related_dict:
+                related_dict[key] = {
+                    **entity,
+                    "_is_direct": is_direct,
+                    "_source_field": source_field,
+                    "_source_entity": source_entity,
+                }
+
+    if entity_type == "Shot":
+        entity_list.append("sg_sequence")
+    elif entity_type == "Asset":
+        entity_list.append("sg_asset_group")
+
+    _add_entity(context.entity, is_direct=True, source_field="context.entity", source_entity=None)
+
+    current = sg.find_one(entity_type, [["id", "is", entity_id]], entity_list)
+    if current:
+        direct_entities = []
+        for field in entity_list:
+            value = current.get(field)
+            if value:
+                entities = value if isinstance(value, list) else [value]
+                for entity in entities:
+                    _add_entity(entity, is_direct=True, source_field=field, source_entity=context.entity)
+                    direct_entities.append(entity)
+
+        for source in direct_entities:
+            if not source:
+                continue
+            indirect = _fetch_indirect_links(sg, source["type"], source["id"])
+            for entity in indirect:
+                _add_entity(entity, is_direct=False, source_field=None, source_entity=source)
+
+    app.logger.info("Related entities:\n%s" % pprint.pformat(related_dict))
+    return list(related_dict.values())
